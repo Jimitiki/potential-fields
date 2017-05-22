85704a694ecb786b1459b83d1cbfbd311bca843a
samuel <samh386@gmail.com>
2017-05-22 10:31:48 -0600

    added fields.py

---
+++ fields.py
@@ -0,0 +1,90 @@
+import math
+
+#class vector:
+#    def __init__(self, x, y):
+#        self.x = x
+#        self.y = y
+
+class field_holder:
+    def __init__(self):
+        self.fields = []
+        
+    def get_field(self, x, y):
+        result_field = (0.0, 0.0)
+        for field in self.fields:
+            result_field += field.get_field(x, y)
+        return result_field
+    
+    def add_field(self, field):
+        self.fields.append(field)
+            
+    
+class linear_repulsive_field:
+    def __init__(self, position_tracker, size, strength):
+        self.position_tracker = position_tracker
+        self.size = size
+        self.strength = strength
+    
+    def get_field(self, x, y):    
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        mag = math.sqrt(dy * dy + dx * dx)        
+        diff = self.size - mag
+        if diff < 0:
+            return (0.0, 0.0)
+        power = self.strength * (diff / size)
+        return (power * math.cos(angle), power * math.sin(angle))
+
+class constant_repulsive_field:
+    def __init__(self, position_tracker, size, strength):
+        self.position_tracker = position_tracker
+        self.size = size
+        self.strength = strength
+    
+    def get_field(self, x, y):    
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        mag = math.sqrt(dy * dy + dx * dx)        
+        if mag > self.size:
+            return (0.0, 0.0)
+        return (self.strength * math.cos(angle), self.strength * math.sin(angle))
+    
+class infinate_constant_attractive_field:
+    def __init__(self, position_tracker, strength):
+        self.position_tracker = position_tracker
+        self.strength = strength       
+       
+    def get_field(self, x, y):
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        return (-self.strength * math.cos(angle), -self.strength * math.sin(angle))        
+
+class fixed_position_tracker:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def get_position(self):
+        return (self.x, self.y)        
+        
+class variable_position_tracker:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def get_position(self):
+        return (self.x, self.y)        
+
+    def set_postion(self, x, y):
+        self.x = x
+        self.y = y        
+        
+h = field_holder()
+h.add_field(constant_repulsive_field(fixed_position_tracker(0, 10), 10, 10))
+print(h.get_field(0, 0))
