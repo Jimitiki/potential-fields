2213033a354dc16dbc4096084f973cee4e720040
samuel <samh386@gmail.com>
2017-05-23 15:38:19 -0600

    robot.py

--- robot.py
+++ robot.py
@@ -5,11 +5,13 @@ def follow_vector(vec_x, vec_y):
     print('following(' + str(vec_x) + ',' + str(vec_y) + ')')
 
 def get_robot_position():
+    location = command.send_command('where robot')
     return (0.0, 0.0)
 
 #set up fields
 f = fields.field_holder()
-f.add_field(fields.infinate_constant_attractive_field(fields.fixed_position_tracker(10, 10), 10))
+position = fields.fixed_position_tracker(10, 10)
+f.add_field(fields.infinate_constant_attractive_field(position, 10))
 
 while True:
     (robot_x, robot_y) = get_robot_position()
