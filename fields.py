e7fe18ac259766003523ffcf80a3199a5e11a8a6
samuel <samh386@gmail.com>
2017-05-24 13:28:27 -0600

    tangential fields

--- fields.py
+++ fields.py
@@ -78,6 +78,78 @@ class random_field:
         angle = random.random() * 2 * math.pi
         return (self.strength * math.cos(angle), self.strength * math.sin(angle))        
 
+class constant_clockwise_tangential_field:
+    def __init__(self, position_tracker, size, strength):
+        self.position_tracker = position_tracker
+        self.size = size
+        self.strength = strength
+    
+    def get_field(self, x, y):
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        angle += math.pi / 2
+        mag = math.sqrt(dy * dy + dx * dx)        
+        if mag > self.size:
+            return (0.0, 0.0)
+        return (self.strength * math.cos(angle), self.strength * math.sin(angle))
+        
+class constant_counterclockwise_tangential_field:
+    def __init__(self, position_tracker, size, strength):
+        self.position_tracker = position_tracker
+        self.size = size
+        self.strength = strength
+    
+    def get_field(self, x, y):
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        angle -= math.pi / 2
+        mag = math.sqrt(dy * dy + dx * dx)        
+        if mag > self.size:
+            return (0.0, 0.0)
+        return (self.strength * math.cos(angle), self.strength * math.sin(angle))
+        
+class linear_clockwise_tangential_field:
+    def __init__(self, position_tracker, size, strength):
+        self.position_tracker = position_tracker
+        self.size = size
+        self.strength = strength
+    
+    def get_field(self, x, y):
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        angle += math.pi / 2
+        mag = math.sqrt(dy * dy + dx * dx)        
+        diff = self.size - mag
+        if diff < 0:
+            return (0.0, 0.0)
+        power = self.strength * (diff / self.size)
+        return (power * math.cos(angle), power * math.sin(angle))
+        
+class linear_counterclockwise_tangential_field:
+    def __init__(self, position_tracker, size, strength):
+        self.position_tracker = position_tracker
+        self.size = size
+        self.strength = strength
+    
+    def get_field(self, x, y):
+        (px, py) = self.position_tracker.get_position()        
+        dy = y - py
+        dx = x - px
+        angle = math.atan2(dy, dx)                
+        angle -= math.pi / 2
+        mag = math.sqrt(dy * dy + dx * dx)        
+        diff = self.size - mag
+        if diff < 0:
+            return (0.0, 0.0)
+        power = self.strength * (diff / self.size)
+        return (power * math.cos(angle), power * math.sin(angle))
+        
 class fixed_position_tracker:
     def __init__(self, x, y):
         self.x = x
@@ -117,8 +189,8 @@ def draw_field(fields, min_x, min_y, max_x, max_y, res_x, res_y):
             x += (max_x - min_x) / (1.0 * res_x)
         print('')
         y += (max_y - min_y) / (1.0 * res_y)
-                    
+
 f = field_holder()
-f.add_field(infinate_constant_attractive_field(fixed_position_tracker(0, 0), 10))
-f.add_field(linear_repulsive_field(fixed_position_tracker(0, 5), 5, 20))
-draw_field(f, -10, -10, 10, 20, 100, 100)
+f.add_field(infinate_constant_attractive_field(fixed_position_tracker(0, 0), 5))
+f.add_field(linear_clockwise_tangential_field(fixed_position_tracker(0, 5), 5, 10))
+draw_field(f, -10, -10, 10, 10, 100, 100)
