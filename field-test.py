9b7781283da88cc43753eb42644b4ffe500e2f42
Jimitiki <cwjohnson42@gmail.com>
2017-05-26 17:05:56 -0600

    messing with strengths of fields

--- field-test.py
+++ field-test.py
@@ -16,16 +16,16 @@ r_field3 = markers[str(24)]["center"]
 r_field4 = markers[str(25)]["center"]
 f = fields.field_holder()
 f.add_field(fields.infinate_constant_attractive_field(fields.fixed_position_tracker(goal[0], goal[1]), 11))
-f.add_field(fields.linear_repulsive_field(fields.fixed_position_tracker(r_field1[0], r_field1[1]), 800, 3))
-f.add_field(fields.linear_repulsive_field(fields.fixed_position_tracker(r_field2[0], r_field2[1]), 1000, 3))
-f.add_field(fields.linear_repulsive_field(fields.fixed_position_tracker(r_field3[0], r_field3[1]), 1200, 4))
-f.add_field(fields.linear_repulsive_field(fields.fixed_position_tracker(r_field4[0], r_field4[1]), 1400, 4))
+f.add_field(fields.linear_counterclockwise_tangential_field(fields.fixed_position_tracker(r_field1[0], r_field1[1]), 800, 6))
+f.add_field(fields.linear_repulsive_field(fields.fixed_position_tracker(r_field2[0], r_field2[1]), 1000, 4))
+f.add_field(fields.linear_repulsive_field(fields.fixed_position_tracker(r_field3[0], r_field3[1]), 1000, 4))
+f.add_field(fields.linear_counterclockwise_tangential_field(fields.fixed_position_tracker(r_field4[0], r_field4[1]), 1400, 5))
 yes = 1
 while yes > 0:
     res = commands.where_robot()
     if "center" in res:
         position = commands.where_robot()["center"]
         vector = f.get_field(position[0], position[1])
-        yes = robot.follow_vector2(vector[0], vector[1], goal)
+        yes = robot.follow_vector(vector[0], vector[1], goal)
 
 commands.close_connection()
--- robot.py
+++ robot.py
@@ -15,43 +15,15 @@ def follow_vector(vec_x, vec_y, goal_pos):
     where = commands.where_robot()
     position = where["center"]
     distance = mathutils.distance(position, goal_pos)
-    if (distance < 30):
-        return 0
-    orientation = where["orientation"]
-
-    angle = mathutils.signed_angle(normal_vector, orientation)
-    magnitude = mathutils.magnitude(vec_x, vec_y)
-    if math.fabs(angle) > 0.15:
-        turn_speed = BASE_TURN_SPEED if angle > 0 else -BASE_TURN_SPEED
-        commands.set_speed(turn_speed , -turn_speed)
-        sleep(max(math.fabs(ROTATE_DELAY * angle), 0.02))
-        commands.set_speed(0, 0)
-        return 1
-    elif magnitude > 0.5:
-        commands.set_speed(BASE_MOVE_SPEED, BASE_MOVE_SPEED)
-        sleep(1)
-        commands.set_speed(0, 0)
-        return 1
-    return 0
-
-def follow_vector2(vec_x, vec_y, goal_pos):
-    print(goal_pos)
-    normal_vector = mathutils.normalize(vec_x, vec_y)
-    where = commands.where_robot()
-    position = where["center"]
-    print(position)
-    distance = mathutils.distance(position, goal_pos)
     print(distance)
-    if (distance < 30):
+    if (distance < 50):
         commands.set_speed(0, 0)
         return 0
     orientation = where["orientation"]
 
     angle = mathutils.signed_angle(normal_vector, orientation)
     magnitude = mathutils.magnitude(vec_x, vec_y)
-    print(magnitude, angle)
     angle = angle / (math.pi) + 1
-    print(angle)
     left = int(round(magnitude * (angle - 0.5)))
     right = int(round(magnitude * (1.5 - angle)))
     print(left, right)
